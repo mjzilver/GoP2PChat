fd4c236db1a41d1bad097af141d35b0bc049643d
Mick Jari Zilverentant <jari.zilverentant@infosupport.nl>
2024-11-05 15:54:59 +0100

    styling added

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type peer struct {
@@ -33,49 +34,53 @@ type model struct {
 	messages []message
 	mu       sync.Mutex
 	peer     peer
+	width    int
+	height   int
 }
 
-const defaultNick = "Anonymous"
+const (
+	title       = "Go P2P UDP Chat"
+	defaultNick = "Anonymous"
+	welcomeMsg  = "Welcome to the Go P2P UDP Chat!"
+	helpMsg     = "Type /nick <nickname> to set your nickname. \n/Connect <peer-address> to connect to a peer. \nPress enter to send a message."
+)
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run main.go <local-addr> <peer-addr>")
-		fmt.Println("Example: go run main.go :3000 :3001")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <listening-port>")
 		return
 	}
 
 	localAddr := os.Args[1]
-	peerAddr := os.Args[2]
 
 	m := model{
+		nick: defaultNick,
 		peer: peer{
-			addr: peerAddr,
 			nick: defaultNick,
 		},
-		nick: defaultNick,
 	}
 
-	// Create the Bubble Tea program
 	p := tea.NewProgram(&m)
 
-	// Start UDP server in a goroutine, passing the program reference
-	go startUDPServer(localAddr, p)
+	go startUDPServer(localAddr, p, &m)
 
 	if _, err := p.Run(); err != nil {
-		fmt.Printf("Error starting program: %v\n", err)
+		m.sendError(fmt.Sprintf("Error running program: %v", err))
 	}
+
+	m.sendError("Program exited")
 }
 
-func startUDPServer(localAddr string, p *tea.Program) {
+func startUDPServer(localAddr string, p *tea.Program, m *model) {
 	addr, err := net.ResolveUDPAddr("udp", localAddr)
 	if err != nil {
-		fmt.Printf("Failed to resolve address %s: %v\n", localAddr, err)
+		m.sendError(fmt.Sprintf("Failed to resolve address %s: %v", localAddr, err))
 		return
 	}
 
 	conn, err := net.ListenUDP("udp", addr)
 	if err != nil {
-		fmt.Printf("Failed to listen on %s: %v\n", localAddr, err)
+		m.sendError(fmt.Sprintf("Failed to listen on %s: %v", localAddr, err))
 		return
 	}
 	defer conn.Close()
@@ -84,19 +89,17 @@ func startUDPServer(localAddr string, p *tea.Program) {
 	for {
 		n, _, err := conn.ReadFromUDP(buf)
 		if err != nil {
-			fmt.Println("Error reading from UDP:", err)
+			m.sendError(fmt.Sprintf("Error reading from UDP: %v", err))
 			continue
 		}
 
-		// Unmarshal the message
 		var msg message
 		err = json.Unmarshal(buf[:n], &msg)
 		if err != nil {
-			fmt.Println("Error unmarshaling message:", err)
+			m.sendError(fmt.Sprintf("Error unmarshaling message: %v", err))
 			continue
 		}
 
-		// Send the message to the Bubble Tea program
 		p.Send(NewMessageMsg{msg: msg})
 	}
 }
@@ -106,6 +109,7 @@ type NewMessageMsg struct {
 }
 
 func (m *model) Init() tea.Cmd {
+	m.sendWelcome()
 	return nil
 }
 
@@ -121,86 +125,173 @@ func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 		case "enter":
 			if m.input != "" {
-				// if starts with / send to commands function
-
-				if strings.HasPrefix(m.input, "/nick ") {
-					m.nick = strings.TrimPrefix(m.input, "/nick ")
-					m.input = ""
-					return m, nil
+				if err := m.handleCommand(); err != nil {
+					m.sendError(err.Error())
 				}
-
-				if strings.HasPrefix(m.input, "/connect") {
-					// connect to a different peer
-					// example: /connect 192.168.1.1:3000
-					peerAddr := strings.TrimPrefix(m.input, "/connect ")
-					m.peer.addr = peerAddr
-
-					m.input = ""
-					return m, nil
-				}
-
-				if m.nick == defaultNick {
-					fmt.Println("Please set a nickname first. Use /nick <nickname>")
-					m.input = ""
-					return m, nil
-				}
-
-				newMsg := message{
-					Text:      m.input,
-					Timestamp: time.Now(),
-					Nick:      m.nick,
-				}
-
-				go sendMessage(m, newMsg)
-
-				m.mu.Lock()
-				m.messages = append(m.messages, newMsg)
-				m.mu.Unlock()
-
 				m.input = ""
 			}
 		default:
 			m.input += msg.String()
 		}
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
 	case NewMessageMsg:
 		m.mu.Lock()
 		m.messages = append(m.messages, msg.msg)
 		m.mu.Unlock()
+
 		return m, nil
 	}
 	return m, nil
 }
 
-func (m *model) View() string {
-	var b strings.Builder
-	b.WriteString("P2P Chat\n\n")
+func (m *model) handleCommand() error {
+	switch {
+	case strings.HasPrefix(m.input, "/nick "):
+		m.nick = strings.TrimPrefix(m.input, "/nick ")
+		return nil
 
-	m.mu.Lock()
-	for _, msg := range m.messages {
-		b.WriteString(msg.String())
-		b.WriteString("\n")
+	case strings.HasPrefix(m.input, "/connect "):
+		peerAddr := strings.TrimPrefix(m.input, "/connect ")
+		m.peer.addr = peerAddr
+		return nil
+
+	case m.input == "/help":
+		m.sendHelp()
+		return nil
+
+	default:
+		if m.nick == defaultNick {
+			return fmt.Errorf("please set a nickname first. Use /nick <nickname>")
+		}
+
+		newMsg := message{
+			Text:      m.input,
+			Timestamp: time.Now(),
+			Nick:      m.nick,
+		}
+
+		go sendMessage(m, newMsg)
+
+		m.mu.Lock()
+		m.messages = append(m.messages, newMsg)
+		m.mu.Unlock()
+		return nil
 	}
-	m.mu.Unlock()
+}
 
-	b.WriteString("\n> " + m.input)
-	return b.String()
+func (m *model) View() string {
+	mainBodyHeight := m.height - 2
+
+	leftWidth := (m.width / 5) * 4
+	rightWidth := m.width - leftWidth
+
+	chatStyle := lipgloss.NewStyle().
+		Width(leftWidth).
+		Height(mainBodyHeight).
+		Background(lipgloss.Color("#1d1f21")).
+		Foreground(lipgloss.Color("#c5c8c6"))
+
+	peerListStyle := lipgloss.NewStyle().
+		Width(rightWidth).
+		Height(mainBodyHeight).
+		Padding(0, 1).
+		Background(lipgloss.Color("#282a36")).
+		Foreground(lipgloss.Color("#50fa7b"))
+
+	inputStyle := lipgloss.NewStyle().
+		Width(m.width).
+		Height(1).
+		Background(lipgloss.Color("#44475a")).
+		Foreground(lipgloss.Color("#50fa7b"))
+
+	titleStyle := lipgloss.NewStyle().
+		Width(m.width).
+		Height(1).
+		Background(lipgloss.Color("#44475a")).
+		Foreground(lipgloss.Color("#f8f8f2")).
+		Bold(true).
+		Italic(true)
+
+	mainBody := lipgloss.JoinHorizontal(lipgloss.Top, chatStyle.Render(m.renderMessages()), peerListStyle.Render(m.renderPeers()))
+
+	return lipgloss.JoinVertical(lipgloss.Top, titleStyle.Render(title), mainBody, inputStyle.Render(m.renderInput()))
+}
+
+func (m *model) renderMessages() string {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	var msgs string
+	for _, msg := range m.messages {
+		msgs += msg.String() + "\n"
+	}
+	return msgs
+}
+
+func (m *model) renderPeers() string {
+	if m.peer.addr == "" {
+		return "No peer connected"
+	}
+	return fmt.Sprintf("Connected peers: \n%s (%s)", m.peer.nick, m.peer.addr)
+}
+
+func (m *model) renderInput() string {
+	return lipgloss.NewStyle().Width(m.width).Render("> " + m.input)
+}
+
+func (m *model) sendError(err string) {
+	m.Update(NewMessageMsg{
+		msg: message{
+			Text:      err,
+			Timestamp: time.Now(),
+			Nick:      "Error",
+		},
+	})
+}
+
+func (m *model) sendHelp() {
+	m.Update(NewMessageMsg{
+		msg: message{
+			Text:      helpMsg,
+			Timestamp: time.Now(),
+			Nick:      "System",
+		},
+	})
+}
+
+func (m *model) sendWelcome() {
+	m.Update(NewMessageMsg{
+		msg: message{
+			Text:      welcomeMsg,
+			Timestamp: time.Now(),
+			Nick:      "System",
+		},
+	})
 }
 
 func sendMessage(m *model, msg message) {
+	if m.peer.addr == "" {
+		m.sendError("No peer address set. Use /connect <peer-address>")
+		return
+	}
+
 	conn, err := net.Dial("udp", m.peer.addr)
 	if err != nil {
-		fmt.Println("Error connecting to peer:", err)
+		m.sendError(fmt.Sprintf("Error connecting to peer: %v", err))
 		return
 	}
 	defer conn.Close()
 
 	msgBytes, err := json.Marshal(msg)
 	if err != nil {
-		fmt.Println("Error marshaling message:", err)
+		m.sendError(fmt.Sprintf("Error marshaling message: %v", err))
 		return
 	}
 	_, err = conn.Write(msgBytes)
 	if err != nil {
-		fmt.Println("Error sending message:", err)
+		m.sendError(fmt.Sprintf("Error sending message: %v", err))
+		return
 	}
 }
